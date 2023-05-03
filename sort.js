37a417c0f035073773a86ca2e10fa8f492d976e1
QYRYI4 <zszoltan811@gmail.com>
2023-05-03 20:25:56 +0200

    added modes

--- sort.js
+++ sort.js
@@ -2,22 +2,25 @@ const shuffle_btn = document.getElementById('shuffle')
 const start_btn = document.getElementById('start_btn')
 const stop_btn = document.getElementById('stop_btn')
 const speed_range = document.getElementById('speed_range')
+const mode_select = document.getElementById('mode_select')
 const bars_container = document.getElementById('bars_container')
 const method_select = document.getElementById('method_select')
-const barHeightMultiplier = 8.5
-const MINRANGE = 1
-const MAXRANGE = 80
-const NUMOFBARS = 100
-let unsortedArray = new Array(NUMOFBARS)
+const barHeightMultiplier = 0.85
+const MINRANGE = 10
+const MAXRANGE = 800
+let numOfBars = mode_select.value.split(',')[0]
+let speed = mode_select.value.split(',')[1]
+let unsortedArray = new Array(numOfBars)
 let isStopped = false
-let speed = 50
 
-const generateRandomNum = (min,max) => {
-    return Math.floor(Math.random() * (max-min +1)) + min;
+
+
+const generateRandomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const delay = (ms) => {
-    return new Promise((sleep) => setTimeout(sleep,ms))
+    return new Promise((sleep) => setTimeout(sleep, ms))
 }
 
 const stopAllFunctions = () => {
@@ -25,8 +28,9 @@ const stopAllFunctions = () => {
 }
 
 const createRandomArray = () => {
-    for(let i = 0; i < NUMOFBARS; i++){
-        unsortedArray[i] = generateRandomNum(MINRANGE,MAXRANGE)
+    unsortedArray = []
+    for (let i = 0; i < numOfBars; i++) {
+        unsortedArray[i] = generateRandomNum(MINRANGE, MAXRANGE)
     }
 }
 
@@ -37,15 +41,20 @@ const setInterface = (val) => { // Val must be boolean!!!
 }
 
 const renderBars = (array) => {
-    for(let i = 0; i < array.length; i++){
+    /* When bars are rendered, in the main case: width = numberofbars/10; margin = 0 numberofbars/50 */
+    for (let i = 0; i < array.length; i++) {
         let bar = document.createElement("div")
         bar.classList.add("bar")
         bar.style.height = array[i] * barHeightMultiplier + 'px'
+        bar.style.width = 1500 / numOfBars + 'px'
+        bar.style.marginLeft = 150 / numOfBars + 'px'
+        bar.style.marginRight = 150 / numOfBars + 'px'
+
         bars_container.appendChild(bar)
     }
 }
 
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
     // Set up (Bar width,etc...)
     createRandomArray()
     renderBars(unsortedArray)
@@ -56,8 +65,7 @@ shuffle_btn.addEventListener('click', () => {
     bars_container.innerHTML = ""
     renderBars(unsortedArray)
 })
-start_btn.addEventListener('click',() => {
-    //TODO! Update with switch!!!
+start_btn.addEventListener('click', () => {
     switch (method_select.value) {
         case 'bubblesort':
             bubbleSort(unsortedArray)
@@ -71,69 +79,70 @@ stop_btn.addEventListener('click', () => {
     isStopped = true
     setInterface(true)
 })
-speed_range.addEventListener('input', (event) => {
-    speed = event.target.value
-    
+mode_select.addEventListener('change', () => {
+    numOfBars = mode_select.value.split(',')[0]
+    speed = mode_select.value.split(',')[1]
+    createRandomArray()
+    bars_container.innerHTML = ""
+    renderBars(unsortedArray)
 })
 
 const bubbleSort = async (arr) => {
     let bars = document.getElementsByClassName('bar')
-    for(let i = 0; i< arr.length; i++){
-        for(let j = 0;j< arr.length-1-i;j++){
-            if(isStopped) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (isStopped) {
                 setInterface(true)
                 isStopped = false
                 return arr
             }
-            if(arr[j] > arr[j+1]){
-                for(let k = 0; k < bars.length;k++){
-                    if(k!== j &&k !== j+1){
+            if (arr[j] > arr[j + 1]) {
+                for (let k = 0; k < bars.length; k++) {
+                    if (k !== j && k !== j + 1) {
                         bars[k].style.backgroundColor = '#FEE715FF'
                     }
                 }
                 let tmp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = tmp
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tmp
                 bars[j].style.height = arr[j] * barHeightMultiplier + "px"
                 bars[j].style.backgroundColor = "aqua"
-                bars[j +1].style.height = arr[j +1] * barHeightMultiplier + "px"
-                bars[j +1].style.backgroundColor = "aqua"
-                await delay((500-speed))
+                bars[j + 1].style.height = arr[j + 1] * barHeightMultiplier + "px"
+                bars[j + 1].style.backgroundColor = "aqua"
+                await delay(speed)
             }
         }
-        await delay(10-speed)
     }
     setInterface(true)
     return arr
-} 
+}
 
 const selectionSort = async (arr) => {
     let bars = document.getElementsByClassName('bar')
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i + 1; j < bars.length;j++){
-            for(let k = 0; k < bars.length;k++){
-                if(k!== j &&k !== i){
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < bars.length; j++) {
+            for (let k = 0; k < bars.length; k++) {
+                if (k !== j && k !== i) {
                     bars[k].style.backgroundColor = '#FEE715FF'
                 }
-            if(isStopped) {
-                setInterface(true)
-                isStopped = false
-                return arr
+                if (isStopped) {
+                    setInterface(true)
+                    isStopped = false
+                    return arr
+                }
+                bars[j].style.height = arr[j] * barHeightMultiplier + "px"
+                bars[j].style.backgroundColor = "aqua"
+                if (arr[j] < arr[i]) {
+
+                    tmp = arr[j]
+                    arr[j] = arr[i]
+                    arr[i] = tmp
+                    bars[i].style.height = arr[i] * barHeightMultiplier + "px"
+                    bars[i].style.backgroundColor = "aqua"
+                }
             }
-            bars[j].style.height = arr[j] * barHeightMultiplier + "px"
-            bars[j].style.backgroundColor = "aqua"
-            if(arr[j] < arr[i]) {
-                
-                tmp = arr[j]
-                arr[j] = arr[i]
-                arr[i] = tmp
-                bars[i].style.height = arr[i] * barHeightMultiplier + "px"
-                bars[i].style.backgroundColor = "aqua"
-            }
-            }
-            await delay((500-speed))
+            await delay(speed)
         }
-        await delay(10-speed)
     }
     setInterface(true)
     return arr
